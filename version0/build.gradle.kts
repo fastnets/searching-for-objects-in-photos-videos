ccad6c31731ae4593e4ec70dbd5fa4fc7f398dc2
fastnets <147835358+fastnets@users.noreply.github.com>
2024-05-08 18:53:28 +0500

    Delete version0 directory
    Delete old version

--- version0/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-buildscript {
-    dependencies {
-        classpath(libs.google.services)
-    }
-}
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-}
